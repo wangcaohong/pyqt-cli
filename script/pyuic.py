cdf9250032bbaaf6bffe683132a1aaacee22bbea
wch <16534421@qq.com>
2019-08-23 17:26:07 +0800

    在脚本中添加自动生成 manager

--- script/pyuic.py
+++ script/pyuic.py
@@ -3,10 +3,15 @@ import os
 def ui_change(target_name):
     ui_path = '../ui/'
     signal = os.system('pyuic5 -o ' + ui_path + target_name + '.py ' + ui_path + target_name + '.ui')
-    if (signal == 0):
+    if signal == 0:
         print('ui change succeeded.')
     else:
         os.system('rm -f ' + ui_path + target_name + '.py')
+    tip = input('Whether to generate Manager? (Yes / No):')
+    if tip == 'Yes' or tip == '' or tip == 'yes' or tip == 'YES':
+        os.system('touch ../' + target_name + 'Manager.py')
+        print('Generated ' + target_name + ' manager succeeded.')
+    print('End of script.')
 
 if __name__ == '__main__':
     target = input('Please enput target ui file name:')
