1033ec8fe0a807a1ef240b2bad893a1586b5a4a2
Crow.Wong <16534421@qq.com>
2019-08-29 23:09:33 +0800

    Update EasyChange.py
    当用vscode运行时，若在不同的目录执行coderunner会提示不存在ui文件夹，这边先cd到脚本目录下在进行转换操作

--- script/EasyChange.py
+++ script/EasyChange.py
@@ -2,13 +2,13 @@ import os
 
 def ui_change(target_name):
     ui_path = '../ui/'
-    signal = os.system('pyuic5 -o ' + ui_path + target_name + '.py ' + ui_path + target_name + '.ui')
+    signal = os.system('cd ../script/ && pyuic5 -o ' + ui_path + target_name + '.py ' + ui_path + target_name + '.ui')
     if signal == 0:
         print('ui change succeeded.')
     else:
         os.system('rm -f ' + ui_path + target_name + '.py')
     tip = input('Whether to generate Manager? (Yes / No):')
-    if tip == 'Yes' or tip == '' or tip == 'yes' or tip == 'YES':
+    if tip == 'Yes' or tip == 'yes' or tip == 'YES':
         os.system('touch ../' + target_name + 'Manager.py')
         print('Generated ' + target_name + ' manager succeeded.')
     print('End of script.')
